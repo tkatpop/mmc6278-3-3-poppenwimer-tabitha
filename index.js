e718b696b1b7e359e710ec766352a39280bb8675
Tabitha Poppenwimer <poppenwimer.tkat@ufl.edu>
2022-05-29 12:26:46 +0300

    questionArr listed

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+var questionsArr = [
+    {
+        question: 'Camels have three sets of eyelids.',
+        answer: true
+    },
+
+    {
+        question: 'A group of monkeys is called a herd.',
+        answer: false
+    },
+
+    {
+        question: 'Crocodiles shed tears when they eat food.',
+        answer: true
+    },
+
+    {
+        question: 'Sharks are mammals.',
+        answer: false
+    },
+
+    {
+        question: 'Sloths take two weeks to digest food.',
+        answer: true
+    },
+
+    {
+        question: 'A group of jellyfish is called a smack.',
+        answer: true
+    },
+
+    {
+        question: 'Spiders have five legs.',
+        answer: false
+    },
+
+    {
+        question: 'The German shepherd is a breed of cat.',
+        answer: false
+    },
+
+    {
+        question: 'Sea otters have favourite rocks that they use to crack open their food.',
+        answer: true
+    },
+
+    {
+        question: 'A penguin is in the same scientific family as a lobster.',
+        answer: false
+    }
+
+]
